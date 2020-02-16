3fd156573aa4c0dd2691b14a3b2aabe697b1f6d7
Astiti Hanum A <astitihanum99@gmail.com>
2020-02-16 10:46:55 +0700

    Latihan 2, membuat halaman welcome, login, register, dan profile

---
+++ app/src/main/java/id/putraprima/marketplacelayout/Personal_InfoActivity.java
@@ -0,0 +1,12 @@
+package id.putraprima.marketplacelayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Personal_InfoActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
